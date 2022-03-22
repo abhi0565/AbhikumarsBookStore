2f0c2b923fb5b8f6d905995fdbd9931c7cc4f631
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-03-21 19:26:50 -0500

    Add project files.

---
+++ AbhikumarsBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AbhikumarsBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
