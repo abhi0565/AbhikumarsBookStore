ef8a83893920b0edd79e48af22e5f7ade8048bc4
abhi0565 <91274937+abhi0565@users.noreply.github.com>
2022-03-29 16:29:53 -0500

    made index.cshtml in category

---
+++ AbhikumarsBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using AbhikumarsBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbhikumarsBookStore.Area.Admin.Controllers
+{
+   
+    public class CategoryController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var allobj = unitOfWork.Category.GetAll();
+            return Json(new { data = allobj });
+        }
+       
+    }
+}
